672c220872ad700f740f4a57298395b9b5859082
Ayush Solanki <ayushsolanki2901@gmail.com>
2024-09-01 18:52:56 +0530

    Fixes

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,19 +1,59 @@
-import { CollectionConfig } from "payload/types";
+import { PrimaryActionEmailHtml } from "@/components/emails/PrimaryAction";
+import { Access, CollectionConfig } from "payload/types";
+
+const adminsAndUsers: Access = ({ req: { user } }) => {
+  if (user.role === "admin") return true;
+  return {
+    id: {
+      equals: user?.id,
+    },
+  };
+};
 
 export const Users: CollectionConfig = {
   slug: "users",
   auth: {
     verify: {
       generateEmailHTML: ({ token }) => {
-        return `<a href=${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}>Verify My email</a>`;
+        return PrimaryActionEmailHtml({
+          actionLabel: "Verify your Account",
+          buttonText: "Verify Account",
+          href: `${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`,
+        });
       },
     },
   },
   access: {
-    read: () => true,
+    read: adminsAndUsers,
     create: () => true,
+    update: ({ req }) => req.user.role === "admin",
+    delete: ({ req }) => req.user.role === "admin",
+  },
+  admin: {
+    hidden: ({ user }) => user.role !== "admin",
+    defaultColumns: ["id"],
   },
   fields: [
+    {
+      name: "products",
+      label: "Product",
+      admin: {
+        condition: () => false,
+      },
+      type: "relationship",
+      relationTo: "products",
+      hasMany: true,
+    },
+    {
+      name: "product_files",
+      label: "Product Files",
+      admin: {
+        condition: () => false,
+      },
+      type: "relationship",
+      relationTo: "product_files",
+      hasMany: true,
+    },
     {
       name: "role",
       defaultValue: "user",
