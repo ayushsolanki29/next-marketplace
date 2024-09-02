2f29be7ad5f8fd7d0cf42c7ecf22bf530d4421ca
Ayush Solanki <ayushsolanki2901@gmail.com>
2024-09-02 15:13:19 +0530

    update

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -6,8 +6,12 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import UserAccountNav from "./UserAccountNav";
 
-const MobileNav = () => {
+interface MobileNavProps {
+  user: any;
+}
+const MobileNav = ({ user }: MobileNavProps) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const pathname = usePathname();
@@ -102,24 +106,30 @@ const MobileNav = () => {
             </div>
 
             <div className="space-y-6 border-t border-gray-200 px-4 py-6">
-              <div className="flow-root">
-                <Link
-                  onClick={() => closeOnCurrent("/sign-in")}
-                  href="/sign-in"
-                  className="-m-2 block p-2 font-medium text-gray-900"
-                >
-                  Sign in
-                </Link>
-              </div>
-              <div className="flow-root">
-                <Link
-                  onClick={() => closeOnCurrent("/sign-up")}
-                  href="/sign-up"
-                  className="-m-2 block p-2 font-medium text-gray-900"
-                >
-                  Sign up
-                </Link>
-              </div>
+              {user ? (
+                <UserAccountNav user={user} />
+              ) : (
+                <>
+                  <div className="flow-root">
+                    <Link
+                      onClick={() => closeOnCurrent("/sign-in")}
+                      href="/sign-in"
+                      className="-m-2 block p-2 font-medium text-gray-900"
+                    >
+                      Sign in
+                    </Link>
+                  </div>
+                  <div className="flow-root">
+                    <Link
+                      onClick={() => closeOnCurrent("/sign-up")}
+                      href="/sign-up"
+                      className="-m-2 block p-2 font-medium text-gray-900"
+                    >
+                      Sign up
+                    </Link>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
